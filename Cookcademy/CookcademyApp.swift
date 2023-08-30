2022515c9aa85baf02fe7906cb69ac6aea7f9b69
Doha <doha.moustafa@hotmail.com>
2023-08-30 15:07:46 +0200

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by doha moustafa on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
